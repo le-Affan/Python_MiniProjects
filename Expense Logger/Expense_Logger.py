5a5dd46fa8505dfa563dd4a0cb79c5b44875850b
le-Affan <shaikhaffan1234@gmail.com>
2025-07-02 20:04:32 +0530

    Moved Expense Logger files into Expense_Logger folder

--- Expense Logger/Expense_Logger.py
+++ Expense Logger/Expense_Logger.py
@@ -1,7 +1,7 @@
 def add_expense():
     n=int(input("Enter the number of expenses you want to log: "))
     i=0
-    with open("expenses.txt","w") as file:
+    with open("expenses.txt", "w") as file:
 
         while i<n:
             description=input("Enter expense description: ")
@@ -12,7 +12,7 @@ def add_expense():
             except ValueError:
                 print("Invalid amount. Please enter a number.")
 
-    with open("expenses.txt","r") as read_file:
+    with open("expenses.txt", "r") as read_file:
         content=read_file.read()
         print(content)
 
